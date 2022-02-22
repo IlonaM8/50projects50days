e66af1d93f30551901933676b2bf145a3fd145d4
IlonaM8 <edoilo.3521@gmail.com>
2022-02-22 13:29:56 +0100

    event keycode project

--- eventKeyCodes/script.js
+++ eventKeyCodes/script.js
@@ -1,2 +1,17 @@
+window.addEventListener('keydown', (event) =>{
+    insert.innerHTML = `
+    <div class="key">
+            ${event.key === ' ' ? 'Space': event.key}
+            <small>event.key</small>
+        </div>
 
+        <div class="key">
+            ${event.keyCode}
+            <small>event.keyCode</small>
+        </div>
 
+        <div class="key">
+            ${event.code}
+            <small>event.code</small>
+        </div>`
+})
