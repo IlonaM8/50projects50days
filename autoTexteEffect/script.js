b6bacc35e348d7224333fc9fe765b2560698a698
IlonaM8 <edoilo.3521@gmail.com>
2022-04-19 22:22:37 +0200

    autoText project

---
+++ autoTexteEffect/script.js
@@ -0,0 +1,23 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'I am back to JavaScript!'
+
+let idx = 1
+let speed = 300/ speedEl.value
+
+writeText()
+
+function writeText() {
+textEl.innerHTML = text.slice(0, idx)
+
+idx++
+
+if(idx > text.length) {
+    idx = 1
+}
+
+setTimeout(writeText, speed)
+
+}
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
