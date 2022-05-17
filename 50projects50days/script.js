76124b67d81d987bfe975ad1bfbcd2ffdaf5c156
IlonaM8 <edoilo.3521@gmail.com>
2022-05-17 15:41:21 +0200

    javascript, css done

---
+++ 50projects50days/script.js
@@ -0,0 +1,9 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
